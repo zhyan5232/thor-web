a71e14ff1833dea5971ebaa5d9ff4ef7d212cf8c
doit <zhyan5232@gmail.com>
2026-06-16 10:45:47 +0800

    fix: Properly pair v-if and v-else in TriggerConfig.vue execType conditional rendering

--- src/views/config/TriggerConfig.vue
+++ src/views/config/TriggerConfig.vue
@@ -1,23 +1,31 @@
-<template>
-  <!-- 间隔时间模式 -->
-  <a-row v-else :gutter="16">
-    <a-col :span="8">
-      <a-form-item label="间隔时间" required>
-        <a-input-number v-model:value="form.intervalValue" :min="0" style="width:100%" @change="generateCron" />
-      </a-form-item>
-    </a-col>
-    <a-col :span="8">
-      <a-form-item label="单位" required>
-        <a-select v-model:value="form.intervalUnit" @change="generateCron">
-          <a-select-option value="秒">秒</a-select-option>
-          <a-select-option value="分">分</a-select-option>
-          <a-select-option value="时">时</a-select-option>
-        </a-select>
-      </a-form-item>
-    </a-col>
-  </a-row>
-</template>
+<!-- 执行时间模式 -->
+<a-row v-if="form.execType === '执行时间'" :gutter="16">
+  <a-col :span="12">
+    <a-form-item label="执行时间" required>
+      <a-time-picker
+        v-model:value="form.execTime"
+        format="HH:mm:ss"
+        value-format="HH:mm:ss"
+        @change="generateCron"
+      />
+    </a-form-item>
+  </a-col>
+</a-row>
 
-<script setup lang="ts">
-// ... 其他代码保持不变 ...
-</script>
+<!-- 间隔时间模式 -->
+<a-row v-else :gutter="16">
+  <a-col :span="8">
+    <a-form-item label="间隔时间" required>
+      <a-input-number v-model:value="form.intervalValue" :min="0" style="width:100%" @change="generateCron" />
+    </a-form-item>
+  </a-col>
+  <a-col :span="8">
+    <a-form-item label="单位" required>
+      <a-select v-model:value="form.intervalUnit" @change="generateCron">
+        <a-select-option value="秒">秒</a-select-option>
+        <a-select-option value="分">分</a-select-option>
+        <a-select-option value="时">时</a-select-option>
+      </a-select>
+    </a-form-item>
+  </a-col>
+</a-row>
